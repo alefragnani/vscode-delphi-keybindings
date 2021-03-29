da3001188a264e97b01687f115e15a41955d6702
Alessandro Fragnani <alefragnani@hotmail.com>
2021-03-28 21:02:27 -0300

    Update whats-new submodule to support v2 and settings sync (Closes #22, Closes #23)

--- src/whats-new/commands.ts
+++ src/whats-new/commands.ts
@@ -6,11 +6,13 @@
 import { commands } from "vscode";
 import { Container } from "../container";
 import { WhatsNewManager } from "../../vscode-whats-new/src/Manager";
-import { WhatsNewDelphiKeybindingsContentProvider } from "./contentProvider";
+import { DelphiKeybindingsContentProvider, DelphiKeybindingsSocialMediaProvider } from "./contentProvider";
 
 export function registerWhatsNew() {
-    const provider = new WhatsNewDelphiKeybindingsContentProvider();
-    const viewer = new WhatsNewManager(Container.context).registerContentProvider("delphi-keybindings", provider);
+    const provider = new DelphiKeybindingsContentProvider();
+    const viewer = new WhatsNewManager(Container.context)
+        .registerContentProvider("alefragnani", "delphi-keybindings", provider)
+        .registerSocialMediaProvider(new DelphiKeybindingsSocialMediaProvider())
     viewer.showPageInActivation();
     Container.context.subscriptions.push(commands.registerCommand('delphiKeybindings.whatsNew', () => viewer.showPage()))
 }
